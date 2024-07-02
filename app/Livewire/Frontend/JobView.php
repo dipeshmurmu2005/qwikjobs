b84efce59f8618563005cfa90dfbc79fa7d1e74a
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-02 21:53:28 +0545

    creates jobview class

---
+++ app/Livewire/Frontend/JobView.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Frontend;
+
+use Livewire\Component;
+
+class JobView extends Component
+{
+    public function render()
+    {
+        return view('livewire.frontend.job-view');
+    }
+}
