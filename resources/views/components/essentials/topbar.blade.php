c23a016ba06dc4c334c1c2f534e985fa0500bf4c
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-02 07:38:58 +0545

    creates topbar component

---
+++ resources/views/components/essentials/topbar.blade.php
@@ -0,0 +1,13 @@
+<div class="top px-12 py-2 bg-black text-white text-xs flex justify-between items-center">
+    <div class="flex items-center">
+        <div class="bell-icon h-6 w-6 mr-1 flex justify-center items-center"><img
+                src="https://cdn.dribbble.com/users/2514055/screenshots/5538470/__.gif" alt=""
+                class="h-full w-full object-cover"></div>
+        <a href="#" class="tracking-wide">Subscribe for job alerts by email</a>
+    </div>
+
+    <div class="flex items-center gap-10">
+        <span><i class="fa-light fa-phone mr-2"></i>(+977) 9815937651</span>
+        <span><i class="fa-light fa-envelope mr-2"></i>support@rojgaarnepal.com</span>
+    </div>
+</div>
