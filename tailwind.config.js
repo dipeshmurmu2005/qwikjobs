e26103c46acd06222024266c9bdd0f1e2b49ed9a
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-02 07:37:56 +0545

    customization tailwind themese

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            'grey-text-color': '#5b5b5b',
+            // 'primary-color': '#007456',
+            'danger-color': '#ff3377',
+            'light-danger-color': '#ff337718',
+            'light-primary-color': '#ECF2F0',
+            // 'light-purple-color': '#f5ecff',
+            // 'purple-color': '#795BC4',
+            'yellow-color': '#FFD75E',
+            'light-yellow-color': '#faf6eb',
+            'skin-color': '#fde7de',
+            'black': '#000000',
+            'white': '#ffffff',
+            'border-color': '#EEEEEE',
+            'greycolor': '#c5c6d0',
+            // 'light-grey-color': '#ededed',
+        },
+    },
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+
+                    "primary": "#007456",
+
+                    'light-primary-color': '#ECF2F0',
+
+                    "secondary": "#795BC4",
+
+                    'light-purple-color': '#f5ecff',
+
+                    "accent": "#007456",
+
+                    "neutral": "#c5c6d0",
+
+                    "base-100": "#ededed",
+
+                    "info": "#0000ff",
+
+                    "success": "#00ff00",
+
+                    "warning": "#00ff00",
+
+                    "error": "#ff0000",
+                },
+            },
+        ],
+    },
+    plugins: [
+        require('daisyui'),
+    ],
+}
