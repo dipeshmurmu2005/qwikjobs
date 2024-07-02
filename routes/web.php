23e62aae2360594325cce588ad36540e8b08c3de
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-02 21:54:23 +0545

    creates job view route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Livewire\Frontend\Joblist;
+use App\Livewire\Frontend\JobView;
 use App\Livewire\Frontend\LandingPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', LandingPage::class)->name('home');
 Route::get('/joblist', Joblist::class)->name('joblist');
+Route::get('/jobs/view', JobView::class)->name('jobs.view');
