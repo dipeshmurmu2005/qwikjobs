881be68d507baee1fec5ae8b1c5ac73dc77e3fe1
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-02 09:25:31 +0545

    creates footer component

---
+++ resources/views/components/essentials/footer.blade.php
@@ -0,0 +1,68 @@
+<div class="footer-container">
+    <div class="top px-12">
+        <ul class="flex gap-32 py-20">
+            <li>
+                <div class="header font-bold mb-5">About Us</div>
+                <div class="line-clamp-3 text-grey-text-color">Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Totam quidem esse placeat rerum
+                    voluptatem doloribus perspiciatis fugiat aliquid reprehenderit ea distinctio ipsa provident,
+                    cupiditate nemo in hic inventore eum vero?</div>
+                <div class="flex flex-col mt-5 text-base text-grey-text-color font-medium gap-2">
+                    <span>T.(+977)-9815937651</span>
+                    <span>E.dipeshmurmu7@gmail.com</span>
+                </div>
+            </li>
+            <li>
+                <div class="header font-bold mb-5">Company</div>
+                <div class="links flex flex-col gap-2 text-grey-text-color whitespace-nowrap">
+                    <a href="#" class="hover:text-primary-color">About us</a>
+                    <a href="#" class="hover:text-primary-color">Carrier</a>
+                    <a href="#" class="hover:text-primary-color">Blogs</a>
+                    <a href="#" class="hover:text-primary-color">FAQ's</a>
+                    <a href="#" class="hover:text-primary-color">Contact</a>
+                </div>
+            </li>
+            <li>
+                <div class="header font-bold mb-5">Services</div>
+                <div class="links flex flex-col gap-2 text-grey-text-color whitespace-nowrap">
+                    <a href="#" class="hover:text-primary-color">Jobs</a>
+                    <a href="#" class="hover:text-primary-color">Companies</a>
+                    <a href="#" class="hover:text-primary-color">Candidates</a>
+                    <a href="#" class="hover:text-primary-color">Pricing</a>
+                    <a href="#" class="hover:text-primary-color">Partner</a>
+                </div>
+            </li>
+            <li>
+                <div class="header font-bold mb-5">Support</div>
+                <div class="links flex flex-col gap-2 text-grey-text-color whitespace-nowrap">
+                    <a href="#" class="hover:text-primary-color duration-300">Privacy & Policy</a>
+                    <a href="#" class="hover:text-primary-color duration-300">Terms of Use</a>
+                    <a href="#" class="hover:text-primary-color duration-300">Help Center</a>
+                    <a href="#" class="hover:text-primary-color duration-300">Updates</a>
+                    <a href="#" class="hover:text-primary-color duration-300">Documentation</a>
+                </div>
+            </li>
+            <li>
+                <div class="header font-bold mb-5">Connect</div>
+                <div class="links flex flex-col gap-2 text-grey-text-color whitespace-nowrap">
+                    <a href="#" class="hover:text-primary-color duration-300 inline-flex gap-2 items-center"><i
+                            class="fa-brands fa-square-facebook"></i>Facebook</a>
+                    <a href="#" class="hover:text-primary-color duration-300 inline-flex gap-2 items-center"><i
+                            class="fa-brands fa-instagram"></i>Instagram</a>
+                    <a href="#" class="hover:text-primary-color duration-300 inline-flex gap-2 items-center"><i
+                            class="fa-brands fa-twitter"></i>Twitter</a>
+                    <a href="#" class="hover:text-primary-color duration-300 inline-flex gap-2 items-center"><i
+                            class="fa-brands fa-linkedin"></i>Linkedin</a>
+                    <a href="#" class="hover:text-primary-color duration-300 inline-flex gap-2 items-center"><i
+                            class="fa-brands fa-youtube"></i>YouTube</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="bottom px-12 py-9 border-t border-[#ededed] text-grey-text-color flex justify-between items-center">
+        <span>&copy; 2024 QwikJob. All Rights Reserved</span>
+        <div class="logo h-[50px] w-[200px]">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Upwork-logo.svg/1200px-Upwork-logo.svg.png" alt="">
+        </div>
+    </div>
+</div>
