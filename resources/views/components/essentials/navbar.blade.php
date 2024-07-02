a909c99cd2f867a7e58aea8bad6bcfc4386ef5cd
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-02 11:51:22 +0545

    adds links to the navbar and remove all the old links

--- resources/views/components/essentials/navbar.blade.php
+++ resources/views/components/essentials/navbar.blade.php
@@ -1,162 +1,23 @@
 <div class="navbar flex justify-between items-center px-12 py-4 text-grey-text-color bg-light-primary-color">
     <div class="flex gap-5 items-center">
-        <a href="">
+        <a href="{{ route('home') }}" wire:navigate>
             <div class="logo h-[70px] w-[200px]"><img
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Upwork-logo.svg/1200px-Upwork-logo.svg.png"
                     alt="" class="h-full w-full object-cover"></div>
         </a>
-
-        <ul class="links font-bold flex gap-8 items-center text-sm">
-            <li class="link hover:text-primary-color"><a>Home</a></li>
-            <li class="dropdown-btn relative py-5">
-                <div class="btn-title cursor-pointer hover:text-primary-color">Jobs<i
-                        class="fa-solid fa-chevron-down ml-1"></i></div>
-                <div class="dropdown-menu transition-opacity duration-300 absolute top-full w-fit h-fit flex gap-5 text-black bg-white shadow-md rounded-md hidden p-5 font-medium"
-                    style="z-index: 3">
-                    <ul class="job-categories min-w-52">
-                        <span class="header font-bold text-base block w-fit mb-4">Popular Categories</span>
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3 whitespace-nowrap">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://s3.amazonaws.com/newblog.psd2html.com/wp-content/uploads/2020/11/01142622/illustration-12.png"
-                                        alt="" class="h-full w-full object-cover">
-                                </div>
-                                <div class="flex flex-col">
-                                    <span>Development & IT</span>
-                                    <span class="text-[#c0c0c0] font-bold">213</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3 whitespace-nowrap">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://cdn-icons-png.flaticon.com/512/5732/5732527.png" alt=""
-                                        class="h-full w-full object-cover">
-                                </div>
-
-                                <div class="flex flex-col">
-                                    <span>Finance & Banking</span>
-                                    <span class="text-[#c0c0c0] font-bold">42</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3 whitespace-nowrap">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://cdn4.iconfinder.com/data/icons/contact-us-set-1-7/64/Contact_us_1-09-512.png"
-                                        alt="" class="h-full w-full object-cover">
-                                </div>
-
-                                <div class="flex flex-col">
-                                    <span>Customer Service</span>
-                                    <span class="text-[#c0c0c0] font-bold">19</span>
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-
-                    <ul class="job-categories min-w-52">
-                        <span class="header font-bold text-base block w-fit mb-4">Jobs in Demands</span>
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3 whitespace-nowrap">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://prepinstadotcom.s3.ap-south-1.amazonaws.com/wp-content/uploads/2022/04/Infosys-Salary-2022-for-freshers.webp"
-                                        alt="" class="h-full w-full object-cover">
-                                </div>
-
-                                <div class="flex flex-col">
-                                    <span>Freshers Jobs</span>
-                                    <span class="text-[#c0c0c0] font-bold">312</span>
-                                </div>
-                            </a>
-                        </li>
-
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3 whitespace-nowrap">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTORwTsAVDr82T0uOibyxb9dIh32Oz3dSUtkXgOTk4jNtAy3lAFdJBSCaAOnxFHrerfpwY&usqp=CAU"
-                                        alt="" class="h-full w-full object-cover">
-                                </div>
-                                <div class="flex flex-col">
-                                    <span>Remote Jobs</span>
-                                    <span class="text-[#c0c0c0] font-bold">125</span>
-                                </div>
-                            </a>
-                        </li>
-
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://cdn-icons-png.flaticon.com/512/2245/2245183.png" alt=""
-                                        class="h-full w-full object-cover">
-                                </div>
-
-                                <div class="flex flex-col">
-                                    <span>Part-time Jobs</span>
-                                    <span class="text-[#c0c0c0] font-bold">13</span>
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li class="dropdown-btn  relative py-5">
-                <div class="btn-title cursor-pointer hover:text-primary-color">Companies<i
-                        class="fa-solid fa-chevron-down ml-1"></i></div>
-                <div class="dropdown-menu transition-opacity duration-300 absolute top-full w-fit h-fit flex gap-5 text-black bg-white shadow-md rounded-md hidden p-5"
-                    style="z-index: 3">
-                    <ul class="job-categories min-w-52">
-                        <span class="header font-bold text-base block w-fit mb-4">Popular Categories</span>
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://png.pngtree.com/png-vector/20220804/ourmid/pngtree-unicorn-logo-icon-vector-illustration-icon-cute-silhouette-vector-png-image_33069342.png"
-                                        alt="" class="h-full w-full object-cover"></div>Unicorn
-                            </a>
-                        </li>
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://cdn-icons-png.flaticon.com/512/1486/1486474.png" alt=""
-                                        class="h-full w-full object-cover"></div>MNC
-                            </a>
-                        </li>
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://cdn-icons-png.flaticon.com/512/8203/8203134.png" alt=""
-                                        class="h-full w-full object-cover"></div>Startups
-                            </a>
-                        </li>
-                    </ul>
-
-                    <ul class="job-categories min-w-52">
-                        <span class="header font-bold text-base block w-fit mb-4">Explore Collections</span>
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://cdn3.iconfinder.com/data/icons/game-competition-outline/64/15_Top_Player_game_competition-512.png"
-                                        alt="" class="h-full w-full object-cover"></div>Top Companies
-                            </a>
-                        </li>
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://cdn-icons-png.flaticon.com/512/2905/2905064.png" alt=""
-                                        class="h-full w-full object-cover"></div>IT Companies
-                            </a>
-                        </li>
-                        <li class="job-category hover:bg-light-primary-color p-3 rounded-md duration-300 text-sm">
-                            <a class="flex items-center gap-3 whitespace-nowrap">
-                                <div class="icon h-12 w-12 rounded-full bg-[#ededed] overflow-hidden"><img
-                                        src="https://fintech.com/wp-content/uploads/2023/06/Fintech-Logo-no-tagline-696x696-1-1024x1024.png"
-                                        alt="" class="h-full w-full object-cover"></div>Fintech Companies
-                            </a>
-                        </li>
-                    </ul>
-
-                </div>
-            </li>
-        </ul>
+        <div class="links text-black">
+            <a href="{{ route('joblist') }}" class="font-semibold" wire:navigate>Jobs</a>
+        </div>
+        <div>
+            <label class="input bg-white input-bordered flex items-center gap-2">
+                <input type="text" class="grow" placeholder="Search" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                    class="h-4 w-4 opacity-70">
+                    <path fill-rule="evenodd"
+                        d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                        clip-rule="evenodd" />
+                </svg>
+            </label>
+        </div>
     </div>
 </div>
