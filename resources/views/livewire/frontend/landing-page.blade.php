64a077673a1c4efef735eaa2dccaedc01a81c282
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-02 09:27:22 +0545

    add sections to the landing page component

--- resources/views/livewire/frontend/landing-page.blade.php
+++ resources/views/livewire/frontend/landing-page.blade.php
@@ -2,4 +2,9 @@
     <x-landingpage.herosection />
     <x-landingpage.portalsection />
     <x-landingpage.categories />
+    <x-landingpage.hiringcompanies />
+    <x-landingpage.latestjobs />
+    <x-landingpage.featuredcompanies />
+    <x-landingpage.helpsection />
+    <x-landingpage.testimonialsection />
 </div>
