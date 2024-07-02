422ad1d9b6236ffd1fa65078f0b35846299d8b24
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-02 07:39:25 +0545

    creates herosection component

---
+++ resources/views/components/landingpage/herosection.blade.php
@@ -0,0 +1,51 @@
+<div class="herosection py-10 px-12 bg-light-primary-color flex items-center justify-between gap-5">
+    <div class="w-[50%]">
+        <h1 class="text-5xl font-extrabold stroke-2 stroke-black tracking-wide leading-snug">Got Talent ?<br>Meet
+            Opportunity</h1>
+        <p class="text-grey-text-color text-xl mt-2">Company reviews. Salaries. Interviews. Jobs.</p>
+
+        <div class="flex mt-5 bg-white input input-bordered h-16 items-center p-2 gap-2">
+            <div class="w-fit">
+                <x-heroicon-o-magnifying-glass class="h-5 w-5 mr-5" />
+            </div>
+            <input type="text" class="h-full w-full" placeholder="What are you looking for">
+            <select class="outline-none cursor-pointer">
+                <option disabled selected>Choose a city</option>
+                <option>Han Solo</option>
+                <option>Greedo</option>
+            </select>
+            <button class="btn btn-primary">Search</button>
+        </div>
+
+        <div class="popular-search mt-5">
+            <span class="flex gap-2 flex-wrap"><span class="text-grey-text-color">Popular Searches:</span><a
+                    href="#" class="hover:text-primary-color duration-300 font-semibold">Prototyping</a><a
+                    href="#" class="hover:text-primary-color duration-300 font-semibold">PHP</a></span>
+        </div>
+        <div class="mt-20">
+            <h1 class="text-grey-text-color text-xl">Trusted by leading brands and startups</h1>
+            <ul class="brands mt-5 flex gap-5">
+                <li class="h-[30px] w-[80px]"><img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Square%2C_Inc._logo.svg/2560px-Square%2C_Inc._logo.svg.png"
+                        alt="" class="h-full w-full object-contain"></li>
+                <li class="h-[30px] w-[80px]"><img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Norton-logo-2021.svg/1280px-Norton-logo-2021.svg.png"
+                        alt="" class="h-full w-full object-contain"></li>
+                <li class="h-[30px] w-[80px]"><img
+                        src="https://spin.atomicobject.com/wp-content/uploads/github-logo.png" alt=""
+                        class="h-full w-full object-contain"></li>
+            </ul>
+        </div>
+    </div>
+    <div class="w-[50%] relative">
+        <div class="image h-[600px] w-full"><img
+                src="https://civi.uxper.co/wp-content/uploads/2022/12/girl-hero-2-2.png" alt=""
+                class="h-full w-full object-contain"></div>
+        <div class="h-[70px] w-[250px] absolute left-0 top-72"><img
+                src="https://civi.uxper.co/wp-content/uploads/2022/12/tip-2.png" alt=""
+                class="h-full w-full object-contain"></div>
+        <div class="h-[150px] w-[300px] absolute right-0 bottom-10 overflow-hidden"><img
+                src="https://civi.uxper.co/wp-content/uploads/2022/12/tip-1-1.png" alt=""
+                class="h-full w-full object-contain"></div>
+    </div>
+</div>
