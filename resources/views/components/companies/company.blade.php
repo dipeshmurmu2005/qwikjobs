99030d4b460dcef370b73c5d5d5932dd45c2b3c6
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-02 09:25:23 +0545

    creates company component

---
+++ resources/views/components/companies/company.blade.php
@@ -0,0 +1,24 @@
+<div class="p-5 bg-white rounded-2xl mb-14 hover:shadow-lg relative">
+    <a href="#" class="">
+        <div>
+            <div class="icon h-14 w-14 rounded-full overflow-hidden flex justify-center items-center">
+                <img src="https://thulo.com/images/feature_variant/155/2cc.jpg" alt=""
+                    class="w-full h-full object-cover">
+            </div>
+        </div>
+        <h1 class="font-bold mt-5 tracking-wide text-base">CG Company</h1>
+        <div class="text-grey-text-color text-sm mt-1 flex gap-5"><span><i
+                    class="fa-sharp fa-light fa-location-dot mr-1"></i>Birtamode</span><span><i
+                    class="fa-thin fa-people-group mr-1"></i>14-30</span><span><i
+                    class="fa-solid fa-star mr-1 text-yellow-color"></i>4.5</span></div>
+        <div class="line-clamp-3 mt-2 text-grey-text-color text-sm">Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Nesciunt, deserunt. Lorem ipsum dolor sit amet.</div>
+        <div class="mt-5"><span
+                class="bg-light-primary-color text-primary-color px-3 py-2 rounded-full text-sm">Electronics</span>
+        </div>
+    </a>
+    <div class="follow-btn absolute top-5 right-5">
+        <button type="submit" class="btn btn-primary rounded-full"><x-heroicon-o-plus-circle
+                class="h-5 w-5" />Follow</button>
+    </div>
+</div>
