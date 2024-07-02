255f37498cd36ffec6ce349b5728cd03ebb88aaa
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-02 07:39:38 +0545

    creates portal section component

---
+++ resources/views/components/landingpage/portalsection.blade.php
@@ -0,0 +1,28 @@
+<div class="portals-section px-12 py-10">
+    <ul class="portals flex gap-10">
+        <li class="bg-skin-color p-10 rounded-2xl flex items-center">
+            <div class="w-[60%]">
+                <h1 class="font-bold text-2xl mb-2">For Employers</h1>
+                <p class="text-grey-text-color">Find professionals from around the world and across all skills.</p>
+                <a href="#"><button class="btn btn-primary px-5 py-3 rounded-full mt-5">Post a job for
+                        Free</button></a>
+            </div>
+            <div class="w-[40%]">
+                <img src="https://civi.uxper.co/wp-content/uploads/2022/12/banner-home-01.svg" alt=""
+                    class="h-full w-full object-cover">
+            </div>
+        </li>
+        <li class="bg-skin-color p-10 rounded-2xl flex items-center">
+            <div class="w-[60%]">
+                <h1 class="font-bold text-2xl mb-2">For Candidate</h1>
+                <p class="text-grey-text-color">Build your professional profile, find new job opportunities.</p>
+                <a href="#"><button class="btn btn-primary px-5 py-3 rounded-full mt-5">Upload your
+                        CV</button></a>
+            </div>
+            <div class="w-[40%]">
+                <img src="https://civi.uxper.co/wp-content/uploads/2022/12/banner-home-02.svg" alt=""
+                    class="h-full w-full object-cover">
+            </div>
+        </li>
+    </ul>
+</div>
