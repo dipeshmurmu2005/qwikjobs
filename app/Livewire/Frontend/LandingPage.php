f8b6f50012d613023cd14f7e761395cabc639300
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-02 07:38:10 +0545

    create landing page livewire components

---
+++ app/Livewire/Frontend/LandingPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Frontend;
+
+use Livewire\Component;
+
+class LandingPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.frontend.landing-page');
+    }
+}
