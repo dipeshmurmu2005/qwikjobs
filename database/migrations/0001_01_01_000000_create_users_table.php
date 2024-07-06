3e1b03d663b0bc270969745eb61940e8a8a994b1
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-06 19:03:02 +0545

    adds type field in users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enum\UserTypeEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('type')->default(UserTypeEnum::JOBSEEKER->value);
             $table->rememberToken();
             $table->timestamps();
         });
