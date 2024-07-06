9d37d03f96cbc1f5cf6e084db010903ccad5acc4
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-06 19:04:42 +0545

    adds client signup route to the web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Frontend\ClientSignup;
 use App\Livewire\Frontend\Joblist;
 use App\Livewire\Frontend\JobView;
 use App\Livewire\Frontend\LandingPage;
@@ -7,6 +8,7 @@ use App\Livewire\Frontend\Signup;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/signup', Signup::class)->name('signup');
+Route::get('/clients/signup', ClientSignup::class)->name('clients.signup');
 Route::get('/', LandingPage::class)->name('home');
 Route::get('/joblist', Joblist::class)->name('joblist');
 Route::get('/jobs/view', JobView::class)->name('jobs.view');
