a6c834251bfad4ecf8cfed2d6132115336811950
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-06 13:02:38 +0545

    adds signup route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use App\Livewire\Frontend\Joblist;
 use App\Livewire\Frontend\JobView;
 use App\Livewire\Frontend\LandingPage;
+use App\Livewire\Frontend\Signup;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/signup', Signup::class)->name('signup');
 Route::get('/', LandingPage::class)->name('home');
 Route::get('/joblist', Joblist::class)->name('joblist');
 Route::get('/jobs/view', JobView::class)->name('jobs.view');
